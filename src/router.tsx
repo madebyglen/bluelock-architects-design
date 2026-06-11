64432340b7b098c8e32e9ca5a0d4265badf07755
GLEN <glen.kimani@student.moringaschool.com>
2026-06-11 21:35:22 +0300

    Add Vercel rewrites

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { Analytics } from "@vercel/analytics/next"
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
